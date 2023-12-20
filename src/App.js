30aa043aadce606da0aa93579db816a06cc1f650
harish <harishnagubadi9190@gmail.com>
2023-12-20 15:00:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Main from "./Main"
+import Navbar from "./Navbar"
+import "./App.css"
+
+
+export default function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Main />
+    </div>
+  )
+}
---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import logo from "./images/react-icon-small.png"
+
+export default function Navbar() {
+    return (
+        <nav>
+        <img src={logo} className="image" alt="logo" />
+        <h1 className="nav-heading">ReactFacts</h1>
+        <h3 className="nav-course">React course - Project 1</h3>
+        </nav>
+    )
+}
